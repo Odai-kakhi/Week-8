16229cab6d53befe35a95cd7b1507e059a89065c
Odai Kakhi <odaikakhi@gmail.com>
2017-04-20 20:59:42 +0200

    Add files via upload

---
+++ HYFClass.js
@@ -0,0 +1,27 @@
+import Student from "./Student";
+import Teacher from "./Teacher";
+
+class HYFClass {
+  constructor(members) {
+    this.members = members || [];
+    let membersIsArray = this.members instanceof Array;
+    if( !membersIsArray){
+      throw new Error ('Members should be an arrya!');
+    };
+  };
+
+    addTeacher(member){
+      this.members.push( new Teacher(member))
+    };
+
+    addStudent(member){
+      this.members.push( new Student(member))
+    };
+  getAllTeacher(){
+      return this.members.filter(member => member instanceof Teacher);
+    };
+    getAllStudent(){
+      return this.members.filter(member => member instanceof Student);
+    };
+
+  };
